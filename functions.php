da506183e5c7537c820b5b7ebc868e55c8c8f403
Dapo-Obembe <obembedapo@gmail.com>
2023-08-09 15:31:03 +0100

    Fixed the WPCS issues

--- functions.php
+++ functions.php
@@ -1,40 +1,59 @@
 <?php
 /**
- * Declaring styles and scripts
+ * Declaring styles and scripts.
  *
- * @return $parent
+ * @package @storefront
+ */
+
+/**
+ * Undocumented function
+ *
+ * @return void
  */
 function parent_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style(
+		'parent-style',
+		get_template_directory_uri() . '/style.css',
+		array(),
+		'1.0.0',
+		'all'
+	);
 }
 add_action( 'wp_enqueue_scripts', 'parent_styles' );
+
 
 /**
  * Target Woocommerce Order Editing Area in the Admin Panel
  *
- * @return $order-link
+ * @param WC_Order $order The order for which to generate the link.
+ * @return void
  */
-
 function customer_order_link( $order ) {
-    $customer_id = $order->get_customer_id();
-    if ($customer_id) {
-        $order_id = $order->get_id();
+	$customer_id = $order->get_customer_id();
+	if ( $customer_id ) {
+		$order_id = $order->get_id();
 
-        $nonce = wp_create_nonce( 'switch_to_user_' . $customer_id );
+		$nonce = wp_create_nonce( 'switch_to_user_' . $customer_id );
 
-        $switch_url = add_query_arg(
-            array(
-                'action' => 'switch_to_user',
-                'user_id' => $customer_id,
-                'nr' => 1,
-                '_wpnonce' => $nonce,
-                'redirect_to' => rawurlencode( home_url( '/my-account/view-order/' . $order_id . '/' ) ),
-            ),
-            wp_login_url()
-        );
+		$switch_url = add_query_arg(
+			array(
+				'action'      => 'switch_to_user',
+				'user_id'     => $customer_id,
+				'nr'          => 1,
+				'_wpnonce'    => $nonce,
+				'redirect_to' => rawurlencode(
+					home_url( '/my-account/view-order/' . $order_id . '/' )
+				),
+			),
+			wp_login_url()
+		);
 
-        echo '<p><a href="' . esc_url( $switch_url ) . '">Open User Order Details Page</a></p>';
-    }
+		echo '<p><a href="' .
+		esc_url( $switch_url ) .
+		'">Open User Order Details Page</a></p>';
+	}
 }
-add_action( 'woocommerce_admin_order_data_after_payment_info', 'customer_order_link' );
-
+add_action(
+	'woocommerce_admin_order_data_after_payment_info',
+	'customer_order_link'
+);
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+if (null == 1 ) {
+    echo '<h2>Hello</h2>';
+}
